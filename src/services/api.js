0776502030d3fa3b76e2460b393df77422d75f92
kindlyman343423 <kindlyman343423@gmail.com>
2021-04-13 20:27:59 -0700

    Add logic to set axios BaseURL to correct environment

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:3001',
+    baseURL: process.env.NODE_ENV === 'production' ? "https://web3news.app/api" : 'http://localhost:3001',
     headers: {
         'Accept': 'application/json'
     }
