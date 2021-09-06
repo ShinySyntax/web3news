6d2ed84a7ca4cb94efbcb7bab72d640293ffbbdf
kindlyman343423 <kindlyman343423@gmail.com>
2021-09-06 14:27:09 -0700

    Fix random video selection by wrapping logic in useEffect

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Plyr from "plyr-react";
 import "../assets/plyr.css";
 
@@ -9,36 +9,42 @@ import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
 
 const VideoPlayer = () => {
   const [isCollapsed, setIsCollapsed] = useState(false);
+  const [video, setVideo] = useState(null);
   const handleClick = () => {
     setIsCollapsed(!isCollapsed);
   };
-  const playerRotation = [
-    {
-      title: "What is Defi?",
-      text: "Find out about the decentralized stock exchange of the future, native to web3. Why should you need a bank to get a loan?",
-      src: DefiWhiteboard,
-      provider: "html5",
-    },
-    {
-      title: "What is Cardano?",
-      text: "The operation system of the internet. Secure, scalable and interoperable proof-of-stake blockchain, with digital identity built in.",
-      src: CardanoWhiteboard,
-      provider: "html5",
-    },
-    {
-      title: "Why web3?",
-      text: "Becuase your entire digital life, personal data and financial information belongs to YOU, not them. Be your own digital custodian in an open source future.",
-      src: DefiWhiteboard,
-      provider: "html5",
-    },
-  ];
 
-  const rndItem = {
-    type: "video",
-    sources: [
-      playerRotation[Math.floor(Math.random() * playerRotation.length)],
-    ],
-  };
+  useEffect(() => {
+    const playerRotation = [
+      {
+        title: "What is Defi?",
+        text: "Find out about the decentralized stock exchange of the future, native to web3. Why should you need a bank to get a loan?",
+        src: DefiWhiteboard,
+        provider: "html5",
+      },
+      {
+        title: "What is Cardano?",
+        text: "The operation system of the internet. Secure, scalable and interoperable proof-of-stake blockchain, with digital identity built in.",
+        src: CardanoWhiteboard,
+        provider: "html5",
+      },
+      {
+        title: "Why web3?",
+        text: "Becuase your entire digital life, personal data and financial information belongs to YOU, not them. Be your own digital custodian in an open source future.",
+        src: DefiWhiteboard,
+        provider: "html5",
+      },
+    ];
+
+    const rndItem = {
+      type: "video",
+      sources: [
+        playerRotation[Math.floor(Math.random() * playerRotation.length)],
+      ],
+    };
+
+    setVideo(rndItem);
+  }, []);
 
   const options = {
     clickToPlay: true,
@@ -46,32 +52,36 @@ const VideoPlayer = () => {
   };
 
   return (
-    <div className="flex flex-col max-w-sm fixed bottom-4 left-4 rounded-xl shadow-white items-center bg-darkblue-800">
-      <div className="h-70 w-96 px-6 py-4">
-        <div className="flex flex-1 pb-2">
-          <div className="flex flex-col pb-2">
-            <div className="font-semibold text-sm text-darkblue-100">
-              {rndItem.sources[0].title}
+    <>
+      {video ? (
+        <div className="flex flex-col max-w-sm fixed bottom-4 left-4 rounded-xl shadow-white items-center bg-darkblue-800">
+          <div className="h-70 w-96 px-6 py-4">
+            <div className="flex flex-1 pb-2">
+              <div className="flex flex-col pb-2">
+                <div className="font-semibold text-sm text-darkblue-100">
+                  {video.sources[0].title}
+                </div>
+                <div className="font-normal text-xs text-darkblue-300">
+                  {video.sources[0].text}
+                </div>
+              </div>
+              <div className="top-0 cursor-pointer text-darkblue-300 hover:text-darkblue-600">
+                <FontAwesomeIcon
+                  icon={isCollapsed ? faPlus : faMinus}
+                  title={
+                    isCollapsed ? "Expand News Player" : "Minimize News Player"
+                  }
+                  onClick={handleClick}
+                />
+              </div>
             </div>
-            <div className="font-normal text-xs text-darkblue-300">
-              {rndItem.sources[0].text}
+            <div className={isCollapsed ? "hidden" : ""}>
+              <Plyr source={video} options={options} disabled={isCollapsed} />
             </div>
           </div>
-          <div className="top-0 cursor-pointer text-darkblue-300 hover:text-darkblue-600">
-            <FontAwesomeIcon
-              icon={isCollapsed ? faPlus : faMinus}
-              title={
-                isCollapsed ? "Expand News Player" : "Minimize News Player"
-              }
-              onClick={handleClick}
-            />
-          </div>
         </div>
-        <div className={isCollapsed ? "hidden" : ""}>
-          <Plyr source={rndItem} options={options} disabled={isCollapsed} />
-        </div>
-      </div>
-    </div>
+      ) : null}
+    </>
   );
 };
 
